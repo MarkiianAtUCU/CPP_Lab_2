1105c196acf8a0ad95b282fe237bc26a916e0d8e
Serhiy Rosovskyy <serhiyrosovskyy@Serhiys-MacBook-Air.local>
2019-03-05 11:27:25 +0200

    added time

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
-#include <fstream>
 #include <iostream>
-#include <stdexcept>
+#include <fstream>
+#include <chrono>
+#include <atomic>
+#include <sstream>
+#include <string>
 #include <vector>
+#include <cstring>
+#include <stdexcept>
 using std::vector;
 using std::string;
+
+
+inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
+{
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    auto res_time = std::chrono::high_resolution_clock::now();
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    return res_time;
+}
+
+
+template<class D>
+inline long long to_us(const D& d) {
+    return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
+}
+
+std::vector<int> transformStringStream(std::vector<string> numbers) {
+    vector<int> output;
+    for (int i = 0; i < numbers.size(); ++i) {
+        std::stringstream parser;
+        parser << numbers[i];
+        int x = 0;
+        parser >> x;
+        output.push_back(x);
+    }
+    return output;
+}
 
 vector<string> read_file(string const &path) {
     vector<string> res;
@@ -24,10 +56,29 @@ vector<string> read_file(string const &path) {
     return res;
 }
 
-int main() {
-    vector< string> input_nums = read_file("../out.txt");
-    std::cout << "Hello, World!" << std::endl;
-    std::string word;
+int main(int argc, char *argv[]) {
+
+    if (argc != 4) {
+        std::cout << "Wrong number of files!" << std::endl;
+        std::cout << "The format is following: ./stringToInt <number of method> <input file> <output file>";
+        return 1;
+    }
+
+    std::vector<string> numbers;
+
+    numbers = read_file(std::string("../") + std::string(argv[2]));
+    if (numbers.size() == 0) {
+        return 1;
+    }
+    auto start = get_current_time_fenced();
+    switch (argv[1][0] - '0') {
+        case 1:
+            std::vector<int> out = transformStringStream(numbers);
+            break;
+    }
+    auto end = get_current_time_fenced();
+
+    std::cout << "Time = " << to_us(end - start) << std::endl;
 
     return 0;
 }
