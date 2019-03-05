fa70f9ef4ead6e1ae7d5c5f443f36f1e7f74db45
Markiian <matsiuk@ucu.edu.ua>
2019-03-05 10:51:03 +0200

    fixed reading from file

--- main.cpp
+++ main.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+using std::vector;
+using std::string;
 
-std::vector<std::pair<std::string,std::string>> read_file(std::string const &path) {
-    std::vector<std::pair<std::string,std::string>> res;
-    std::string num_1, num_2;
+vector<string> read_file(string const &path) {
+    vector<string> res;
+    string num_1;
 
     std::ifstream file(path);
     if (file.is_open()) {
-        while (file >> num_1 >> num_2) {
-            std::pair<std::string, std::string> value_pair(num_1, num_2);
-            res.push_back(value_pair);
-
+        while (file >> num_1) {
+            res.push_back(num_1);
         }
         file.close();
     } else {
@@ -25,7 +25,7 @@ std::vector<std::pair<std::string,std::string>> read_file(std::string const &pat
 }
 
 int main() {
-    std::vector<std::pair<std::string, std::string>> input_nums = read_file("../out.txt");
+    vector< string> input_nums = read_file("../out.txt");
     std::cout << "Hello, World!" << std::endl;
     std::string word;
 
